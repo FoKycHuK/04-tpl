2e8a58a982d08e88b97f067ce0cf18585b433fbc
FoKycHuK <fokychuk47@ya.ru>
2015-03-09 00:42:30 +0600

    refucktoring

--- JapaneseCrossword/FileWorker.cs
+++ JapaneseCrossword/FileWorker.cs
@@ -15,29 +15,30 @@ namespace JapaneseCrossword
             {Cell.Black, '*'},
             {Cell.Whilte, '.'}
         };
+
         public static Crossword ReadFromFile(string fileName)
         {
             var lines = File.ReadAllLines(fileName);
 
             var rowCount = int.Parse(lines[0].Split(':')[1].Trim());
-            var rows = new List<int>[rowCount];
-            for (var i = 1; i <= rowCount; i++)
-            {
-                rows[i - 1] = new List<int>();
-                foreach (var value in lines[i].Split(' '))
-                    rows[i - 1].Add(int.Parse(value));
-            }
+            var rows = GetLinesSettings(lines, rowCount, 1);
 
             var columnCount = int.Parse(lines[rowCount + 1].Split(':')[1].Trim());
-            var columns = new List<int>[columnCount];
-            for (var i = rowCount + 2; i < lines.Length; i++)
-            {
-                columns[i - (rowCount + 2)] = new List<int>();
-                foreach (var value in lines[i].Split(' '))
-                    columns[i - (rowCount + 2)].Add(int.Parse(value));
-            }
+            var columns = GetLinesSettings(lines, columnCount, rowCount + 2);
 
             return new Crossword(rows, columns);
+        }
+
+        static List<int>[] GetLinesSettings(string[] lines, int count, int startValue)
+        {
+            var ans = new List<int>[count];
+            for (var i = 0; i < count; i++)
+            {
+                ans[i] = new List<int>();
+                foreach (var value in lines[i + startValue].Split(' '))
+                    ans[i].Add(int.Parse(value));
+            }
+            return ans;
         }
 
         public static void WriteToFile(string fileName, Cell[,] field)
